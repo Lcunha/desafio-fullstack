6b30d80a18b6d24fa8909a76efbb940f3827fef7
Lcunha <lucascarvalho.esw@gmail.com>
2023-10-01 00:49:16 -0300

    #4 #6 # 5 Adicionandoconfiguração das urls das views

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,3 +1,9 @@
+from django.urls import path
+from . import views
+
 urlpatterns = [
-    
+    path("", views.ListarTarefasView.as_view(), name="index"),
+    path("tarefa/add", views.TarefaCreate.as_view(), name="tarefa-add"),
+    path("tarefa/<int:pk>/", views.TarefaUpdate.as_view(), name="tarefa-update"),
+    path("tarefa/<int:pk>/delete/", views.TarefaDelete.as_view(), name="tarefa-delete"),    
 ]
