b56067615fe2990cfcbf997bbba22abec7b42ce7
Lcunha <lucascarvalho.esw@gmail.com>
2023-10-01 18:57:16 -0300

    #7 Incluindo path para reordenacao de tarefas

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("", views.ListarTarefasView.as_view(), name="index"),
     path("tarefa/add", views.TarefaCreate.as_view(), name="tarefa-add"),
     path("tarefa/<int:pk>/", views.TarefaUpdate.as_view(), name="tarefa-update"),
-    path("tarefa/<int:pk>/delete/", views.TarefaDelete.as_view(), name="tarefa-delete"),    
+    path("tarefa/<int:pk>/delete/", views.TarefaDelete.as_view(), name="tarefa-delete"),
+    path("reordenar_tarefas/", views.reordenar_tarefas, name='reordenar_tarefas'),    
 ]
