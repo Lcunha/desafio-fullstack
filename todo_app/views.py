9b5b849c24c57f47c5280aac4c94a3e3a1428694
Lcunha <lucascarvalho.esw@gmail.com>
2023-10-01 00:47:14 -0300

    #4 #6 Criando classes e templates de criar, atualizar e deletar tarefas

--- todo_app/views.py
+++ todo_app/views.py
@@ -12,6 +12,49 @@ from .models import Tarefa
 class ListarTarefasView(ListView):
     model = Tarefa
     template_name = "todo_app/index.html"
-    
 
+class TarefaCreate(CreateView):
+    model = Tarefa
+    template_name = "todo_app/tarefa_form.html"
+    fields = [
+        "nome",
+        "descricao",
+        "realizado",
+        "prioridade",       
+    ]
+    
+    def get_context_data(self):
+        context = super(TarefaCreate, self).get_context_data()
+        context["titulo"] = "Create a new item"
+        return context
+    
+    def get_sucess_url(self):
+        return reverse("index")
+    
+class TarefaUpdate(UpdateView):
+    model = Tarefa
+    fields = [
+        "nome",
+        "descricao",
+        "realizado",
+        "prioridade",
+    ]
+
+    def get_context_data(self):
+        context = super(TarefaUpdate, self).get_context_data()
+        context["titulo"] = "Editar Tarefa"
+        return context
+    
+    def get_sucess_url(self):
+        return reverse("index")
+    
+class TarefaDelete(DeleteView):
+    model = Tarefa
+
+    def get_success_url(self):
+        return reverse_lazy("index")
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
 
